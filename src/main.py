da2fe9163f4c08f9097ae909e0c1a93ec4c0dda3
mrgick <45306973+mrgick@users.noreply.github.com>
2022-02-06 15:45:42 +0300

    add request limit per second

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from src.db.services import initialize_storage
 from src.api.update import get_updates
 from src.api.group import get_groups
 from src.cron import run_cron
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 origins = ["*"]
 
@@ -27,34 +33,41 @@ async def startup_event():
 
 
 @app.get("/")
-async def main_page():
+@limiter.limit("5/second")
+async def main_page(request: Request, response: Response):
     return FileResponse('src/static/index.html')
 
 
 @app.get("/ping")
-async def test_connection():
+@limiter.limit("5/second")
+async def test_connection(request: Request, response: Response):
     return {"result": "true"}
 
 
 @app.get("/update")
-async def get_update(last: Optional[bool] = Query(
-    None,
-    description="If enable - you will get the last update information." +
+@limiter.limit("5/second")
+async def get_update(
+    request: Request,
+    response: Response,
+    last: Optional[bool] = Query(
+        None,
+        description="If enable - you will get the last update information." +
         "<br>If not - you will get all update information.")):
     return await get_updates(last)
 
 
 @app.get("/groups")
-async def groups(
-        list_of_names: Optional[str] = Query(
-            None,
-            enum=["list", "structure"],
-            description="list - you will get list of groups" +
-            "<br> structure - you will get structure of groups"),
-        name: Optional[str] = Query(
-            None,
-            description="Name of group, that you want to get." +
-            " Example - 0432-06" +
-            "<br> all - you will get all groups with details."
-        )):
+@limiter.limit("5/second")
+async def groups(request: Request,
+                 response: Response,
+                 list_of_names: Optional[str] = Query(
+                     None,
+                     enum=["list", "structure"],
+                     description="list - you will get list of groups" +
+                     "<br> structure - you will get structure of groups"),
+                 name: Optional[str] = Query(
+                     None,
+                     description="Name of group, that you want to get." +
+                     " Example - 0431-06" +
+                     "<br> all - you will get all groups with details.")):
     return await get_groups(list_of_names, name)
